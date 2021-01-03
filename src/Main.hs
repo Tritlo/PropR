c04e9ac6c1850d9dbb606635f911f509a0092d0f
Matthias Pall Gissurarson <mpg@mpg.is>
2021-01-03 16:31:27 +0000

    Run quickcheck on a given expression and type

--- src/Main.hs
+++ src/Main.hs
@@ -21,15 +21,9 @@ import Control.Monad.IO.Class
 
 import System.Process
 
-
-importStmts = ["import Prelude"]
-packages = [ExposePackage "base" (PackageArg "base") (ModRenaming True [])]
-holeFlags = [ Opt_ShowHoleConstraints
-            , Opt_ShowMatchesOfHoleFits
-            , Opt_ShowProvOfHoleFits
-            , Opt_ShowTypeAppVarsOfHoleFits
-            , Opt_ShowTypeAppOfHoleFits
-            , Opt_ShowTypeOfHoleFits ]
+import Data.Dynamic
+import Data.List
+import Test.QuickCheck
 
 config :: DynFlags -> DynFlags
 config sflags =
@@ -57,7 +51,7 @@ output p = do
 
 type ValsAndRefs = ([String], [String])
 
-inspectException :: SourceError -> Ghc (Either [ValsAndRefs] HValue)
+inspectException :: SourceError -> Ghc (Either [ValsAndRefs] Dynamic)
 inspectException err = do
     flags <- getSessionDynFlags
     printException err
@@ -76,16 +70,16 @@ inspectException err = do
         spl v = (map (dropWhile (== ' ')) vals, map (dropWhile (== ' ')) rfs)
           where (vals,r:rfs) = break isValid (tail v)
         valsAndRefs = map spl $ map lines $ catMaybes valids
-    liftIO $ print $ valids
-    mapM_ (liftIO . print) valsAndRefs
+    --liftIO $ print $ valids
+    --mapM_ (liftIO . print) valsAndRefs
     return $ Left $ valsAndRefs
     --(map (map (showSDocOneLine flags) . filter (isValid . showSDoc flags)) supp)
 
-evalOrHoleFits :: String -> Ghc (Either [ValsAndRefs] HValue)
+evalOrHoleFits :: String -> Ghc (Either [ValsAndRefs] Dynamic)
 evalOrHoleFits str = do
    initGhcCtxt
    -- Then we can actually run the program!
-   handleSourceError inspectException (compileExpr str >>= (return . Right))
+   handleSourceError inspectException (dynCompileExpr str >>= (return . Right))
 
 getLibDir :: IO (Maybe String)
 getLibDir = do ld <- readCreateProcess (shell "ghc --print-libdir") ""
@@ -93,16 +87,69 @@ getLibDir = do ld <- readCreateProcess (shell "ghc --print-libdir") ""
                           [ld] -> Just ld
                           _ -> Nothing
 
-try :: String -> IO ()
+try :: String -> IO (Either [ValsAndRefs] Dynamic)
 try str = do
    libDir <- getLibDir
    r <- runGhc libDir $ evalOrHoleFits str
    print r
+   return r
+
+tryAtType :: String -> String -> IO (Either [ValsAndRefs] Dynamic)
+tryAtType str ty = try ("((" ++ str ++ ") :: " ++ ty ++ ")")
 
 
+tyToTry = "[Int] -> Int"
+props = ["propIsSum f xs = f xs == sum xs"]
+propNames = map (head . words) props
+propCheckExpr pname = "quickCheckResult (" ++ pname ++ " exprToCheck__)"
+propToLet p = "    " ++ p
+
+buildCheckExprAtTy expr ty =
+     unlines [ "let "
+             , unlines (map ("    " ++) props)
+             , "    exprToCheck__ = (" ++ expr ++ " :: " ++ ty ++ ")"
+             , "    propsToCheck__ = [" ++ (intercalate "," $ map propCheckExpr propNames) ++ "]"
+             , "in ((sequence propsToCheck__) :: IO [Result])"]
+
+
+runCheck :: Either [ValsAndRefs] Dynamic -> IO Bool
+runCheck (Left _) = return False
+runCheck (Right dval) =
+     case fromDynamic dval of
+         Nothing -> return False
+         Just res -> do r <- (res :: IO [Result])
+                        print r
+                        return $ all isSuccess r
+
+toPkg :: String -> PackageFlag
+toPkg str = ExposePackage ("-package "++ str) (PackageArg str) (ModRenaming True [])
+
+importStmts = ["import Prelude", "import Test.QuickCheck"]
+-- All the packages here need to be *globally* available. We should fix this
+-- by wrapping it in e.g. a nix-shell or something.
+packages = map toPkg ["base", "process", "QuickCheck"]
+
+holeFlags = [ Opt_ShowHoleConstraints
+            , Opt_ShowMatchesOfHoleFits
+            , Opt_ShowProvOfHoleFits
+            , Opt_ShowTypeAppVarsOfHoleFits
+            , Opt_ShowTypeAppOfHoleFits
+            , Opt_ShowTypeOfHoleFits ]
 
 main :: IO ()
 main = do
-    try "let thisIsAnExtremelyLongNameOfAFunctionThatIAmHopingWillBreakAndOhMyGodIHaveToMakeItEvenLongerICannotBelieveThisIsItEvenPossible False = True in (_ (_ :: Bool)) :: Bool"
-    try "True"
+    -- try "let thisIsAnExtremelyLongNameOfAFunctionThatIAmHopingWillBreakAndOhMyGodIHaveToMakeItEvenLongerICannotBelieveThisIsItEvenPossible False = True in (_ (_ :: Bool)) :: Bool"
+    -- res <- try "True" -- base case. Returns Right <<Bool>>
+    --res <- try "succ" -- does not work, we need a concrete type if we want
+                        -- a dynamic  i.e. something Typeable. Returns Left []
+    -- res <- try "succ :: Bool -> Bool" -- returns Right <<Bool -> Bool>>
+    -- res2 <- tryAtType "succ" "Bool -> Bool" -- returns Right <<Bool -> Bool>>
+    -- print res2
+    -- res <- tryAtType exprToTry tyToTry
+    -- print res
+    r2 <- try (buildCheckExprAtTy "product" "[Int] -> Int")
+    print r2
+    r3 <- runCheck r2
+    print r3
+
     --putStrLn "ghc-synth!"
