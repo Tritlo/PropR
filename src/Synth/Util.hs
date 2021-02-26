639d9f766648e977684d90bc40743e1f44b33a22
Matthias Pall Gissurarson <mpg@mpg.is>
2021-02-26 17:18:45 +0000

    Add comments and tests for utils

--- src/Synth/Util.hs
+++ src/Synth/Util.hs
@@ -5,23 +5,29 @@ import Control.Monad (when)
 import System.Environment ( getArgs )
 import Data.Char (isSpace)
 
+-- Removes whitespace before and after a string
 trim :: String -> String
 trim = reverse . dropWhile isSpace . reverse . dropWhile isSpace
 
+-- Checks if the debug flag is set
 hasDebug :: IO Bool
 hasDebug = ("-fdebug" `elem`) <$> getArgs
 
+-- Prints only when debug is enabled
 pr_debug :: String -> IO ()
 pr_debug str = do dbg <- hasDebug
                   when dbg $ putStrLn str
 
+-- Prints a string, and then flushes, so that intermediate strings show up
 putStr' :: String -> IO ()
 putStr' str = putStr str >> hFlush stdout
 
+-- Drop a prefix of a string, i.e. dropPrefix "123" "123abc" == "abc"
 dropPrefix :: String -> String -> String
 dropPrefix (p:ps) (s:ss) | p == s = dropPrefix ps ss
 dropPrefix _ s = s
 
+-- Checks if a string starts with a given prefix
 startsWith :: String -> String -> Bool
 startsWith [] _ = True
 startsWith (p:ps) (s:ss) | p == s = startsWith ps ss
--- tests/Tests.hs
+++ tests/Tests.hs
@@ -1,33 +1,48 @@
+{-# LANGUAGE NumericUnderscores #-}
 module Main where
 
 import Test.Tasty
 import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
 
 import Synth.Repair (repair)
-import Synth.Util (trim)
 import Synth.Eval (CompileConfig(..))
+import Synth.Util
+
 
 
 tests :: TestTree
-tests = testGroup "Tests" [unitTests]
+tests = testGroup "Tests" [utilTests, repairTests]
 
-unitTests = testGroup "Unit tests" [
+
+utilTests :: TestTree
+utilTests = testProperties "Utils" [
+      ("dropPrefix", property $ prop_dropsPrefix)
+    , ("startsWith", property $ prop_startsWith)
+    ]
+  where prop_dropsPrefix :: String -> String -> Bool
+        prop_dropsPrefix st rest = dropPrefix st (st++rest) == rest
+        prop_startsWith :: String -> String -> Bool
+        prop_startsWith st rest = startsWith st (st ++ rest) == True
+
+repairTests = testGroup "Repair" [
     -- A simple tests to see if we can repair (foldl (-) 0) to (foldl (+) 0)
-    testCase "Repair 'foldl (-) 0'" $ do
-        let cc = CompConf {
-                    hole_lvl=2,
-                    packages = ["base", "process", "QuickCheck" ],
-                    importStmts = ["import Prelude hiding (id, ($), ($!), asTypeOf)"]}
-            ty = "[Int] -> Int"
-            wrong_prog = "(foldl (-) 0)"
-            props = ["prop_isSum f xs = f xs == sum xs"]
-            context = [ "zero = 0 :: Int"
-                      , "one = 1 :: Int"
-                      , "add = (+) :: Int -> Int -> Int"]
-            expected = "((foldl (+) 0)) :: [Int] -> Int"
-        fixes <- map trim <$> repair cc props context ty wrong_prog
-        expected `elem` fixes @? "Expected repair not found in fixes"
-
+    -- in a reasonable amount of time (here 10s)
+      localOption (mkTimeout 10_000_000) $
+        testCase "Repair 'foldl (-) 0'" $ do
+          let cc = CompConf {
+                      hole_lvl=2,
+                      packages = ["base", "process", "QuickCheck" ],
+                      importStmts = ["import Prelude hiding (id, ($), ($!), asTypeOf)"]}
+              ty = "[Int] -> Int"
+              wrong_prog = "(foldl (-) 0)"
+              props = ["prop_isSum f xs = f xs == sum xs"]
+              context = [ "zero = 0 :: Int"
+                        , "one = 1 :: Int"
+                        , "add = (+) :: Int -> Int -> Int"]
+              expected = "((foldl (+) 0)) :: [Int] -> Int"
+          fixes <- map trim <$> repair cc props context ty wrong_prog
+          expected `elem` fixes @? "Expected repair not found in fixes"
     ]
 
 
