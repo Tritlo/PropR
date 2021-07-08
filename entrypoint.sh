f400a6af319eb083b091d1e5d68188cf04f7123f
Leonhard Applis <Leonhard.Applis@Protonmail.com>
2021-07-08 12:10:11 +0200

    Extensible Entrypoint per Argument found

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
+echo "Starting Astor - Building the command"
 
-cabal run endemic -- $REPAIR_TARGET --log=$LOG_LEVEL
+# As the command gets maybe complex, 
+# it is stitched together with any flag set in docker. 
+
+# Repairtarget must be set, otherwise it makes little sense? 
+commandCollector="cabal run endemic -- $REPAIR_TARGET"
+
+if [ -z ${LOG_LEVEL+x} ]; 
+then echo "Log-Level is not set"; 
+else commandCollector="$commandCollector --log=$LOG_LEVEL "; 
+fi
+
+echo "final cmd is ${commandCollector}"
+
+/bin/bash -c "$commandCollector"
+
+# To keep the container open for inspection
+# echo "Keeping Container open for inspection"
+# tail -f /dev/null
