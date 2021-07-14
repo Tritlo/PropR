06ded8c17f73c58d097b200b18b420ccc0494a97
Matthías Páll Gissurarson <mpg@mpg.is>
2021-07-14 17:25:02 +0000

    [Feature] Use proper argument parsing

--- src/Main.hs
+++ src/Main.hs
@@ -6,8 +6,6 @@
 module Main where
 
 import Control.Monad (when)
-import Data.Aeson
-import qualified Data.ByteString.Lazy as BS
 import Data.Default (def)
 import Data.IORef (IORef, atomicModifyIORef', readIORef, writeIORef)
 import Data.Map.Strict (Map)
@@ -27,23 +25,75 @@ import GhcPlugins (noLoc)
 import System.Directory (createDirectory)
 import System.Environment (getArgs)
 import System.IO
+import System.Random
+
+import Options.Applicative
+
+optParser :: ParserInfo CLIOptions
+optParser = info (cliOpts <**> helper) modinfo
+  where locParse = optional $ (flag' True ( long "log-loc"
+                                    <> help "Add location to log messages"
+                                    ))
+                         <|> (flag' False ( long "no-log-loc"
+                                    <> help "Remove locations from log messages"
+                                    <> internal ))
+        tsParse = optional $ (flag' True ( long "log-timestamp"
+                                    <> help "Add timestamps to log messages"
+                                    <> internal ))
+                         <|> (flag' False ( long "no-log-timestamp"
+                                    <> help "Remove timestamps from log messages"))
+        lvlParse = optional $ option auto ( long "log-level"
+                                         <> metavar "LOGLEVEL"
+                                         <> value WARN
+                                         <> showDefault
+                                         <> help "The logging level to use" )
+        logFileParse = optional $ strOption  (  long "log-file"
+                                             <> metavar "FILE"
+                                             <> help "Append logs to FILE")
+        randSeed = optional $ option auto ( long "seed"
+                                          <> metavar "INT"
+                                          <> help ("The random seed to use. "
+                                                 ++ "Generated at runtime if not provided."))
+        confParse = optional $ strOption ( long "config"
+                                         <> metavar "CONFIG"
+                                        <> help ("The configuration to use. "
+                                                ++ fileJsonDesc))
+        fileJsonDesc = "CONF can either be a path to a JSON file, "
+                    ++ "or the JSON can be specified directly"
+        overrideParse = optional $
+             strOption ( long "override"
+                      <> metavar "CONFIG"
+                      <> help ( "Override the configuration with the given CONFIG. "
+                             ++ fileJsonDesc) )
+        targetsParse = strArgument (metavar "TARGET")
+        cliOpts = CLIOptions <$> locParse <*>
+                                 tsParse <*>
+                                 lvlParse <*>
+                                 logFileParse <*>
+                                 randSeed <*>
+                                 confParse <*>
+                                 overrideParse <*>
+                                 targetsParse
+        modinfo = (briefDesc
+            <> progDesc "Repair TARGET using the endemic genetic method"
+            <> header "endemic - Genetic program repair for Haskell")
+
+
 
 main :: IO ()
 main = do
-  args <- Map.fromList . map (break (== '=')) <$> getArgs
-  conf@Conf {..} <- getConfiguration (tail <$> (args Map.!? "--config"))
-
+  opts@CLIOptions{..} <- execParser optParser
+  conf@Conf {..} <- getConfiguration opts
   -- Set the global flags
   setGlobalFlags conf
 
-  [toFix] <- filter (not . (==) "-" . take 1) <$> getArgs
-  (_, modul, probs) <- moduleToProb compileConfig toFix Nothing
+  (_, modul, probs) <- moduleToProb compileConfig optTarget Nothing
   let (tp@EProb {..} : _) = if null probs then error "NO TARGET FOUND!" else probs
       RProb {..} = detranslate tp
   logStr INFO $ "TARGET:"
-  logStr INFO $ ("  `" ++ r_target ++ "` in " ++ toFix)
+  logStr INFO $ ("  `" ++ r_target ++ "` in " ++ optTarget)
   logStr VERBOSE $ "CONFIG:"
-  logStr VERBOSE $ show (encode conf)
+  logStr VERBOSE $ show conf
   logStr VERBOSE $ "SCOPE:"
   mapM_ (logStr VERBOSE . ("  " ++)) (importStmts compileConfig)
   logStr INFO $ "TARGET TYPE:"
@@ -56,8 +106,11 @@ main = do
   logStr VERBOSE $ showUnsafe e_prog
 
   logStr INFO "REPAIRING..."
-  desc <- describeProblem conf toFix
-  (t, fixes) <- time $ runGenMonad def desc 69420 geneticSearchPlusPostprocessing
+  desc <- describeProblem conf optTarget
+  seed <- case optRandomSeed of
+            Just s -> return s
+            _ -> randomIO
+  (t, fixes) <- time $ runGenMonad def desc seed geneticSearchPlusPostprocessing
   let newProgs = map (`replaceExpr` progAtTy e_prog e_ty) fixes
       fbs = map getFixBinds newProgs
   -- Here we write the found solutions to respective files, we just number them 1 .. n
