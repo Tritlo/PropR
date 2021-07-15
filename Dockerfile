afc5b2c95c807727166fe5cf6e026401ef9cecaa
Leonhard Applis <Leonhard.Applis@Protonmail.com>
2021-07-15 15:25:34 +0200

    updated Dockerfile and added a compose yaml utilizing config

--- Dockerfile
+++ Dockerfile
@@ -8,14 +8,18 @@ COPY ./Endemic.cabal /app/
 COPY ./entrypoint.sh /app/
 RUN chmod +x /app/entrypoint.sh
 
-#TODO: Can we freeze the cabal update here? 
+#TODO: Can we freeze the cabal update here somehow? 
 RUN cabal update
-RUN cabal install --lib QuickCheck 
+RUN cabal install --lib QuickCheck tasty tasty-hunit
+# After Commit acd14d5e7e1cd3d88f072dcda40f0a00ae605daa
+#RUN cabal install check-helpers
 
 RUN cabal build
 
 ENV LOG_LEVEL=INFO
 ENV REPAIR_TARGET=/input
+ENV CONFIG_FILE="/config/config.json"
+ENV LOG_FILE="/output/docker-endemic.log"
 
 # We create the default here, but if you mount it with docker compose
 # It will be created if not existing.
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,40 @@
 #!/bin/bash
-echo "Starting Astor - Building the command"
+echo "Starting Repair-Docker - Building the command"
 
 # As the command gets maybe complex, 
 # it is stitched together with any flag set in docker. 
 
-# Repairtarget must be set, otherwise it makes little sense? 
+# Repairtarget must be set, otherwise it makes little sense
 commandCollector="cabal run endemic -- $REPAIR_TARGET"
 
 if [ -z ${LOG_LEVEL+x} ]; 
 then echo "Log-Level is not set"; 
-else commandCollector="$commandCollector --log=$LOG_LEVEL "; 
+else commandCollector="$commandCollector --log-level=$LOG_LEVEL "; 
+fi
+
+if [ -z ${LOG_FILE+x} ]; 
+then echo "Log-File is not set"; 
+else commandCollector="$commandCollector --log-file=$LOG_FILE "; 
+fi
+
+if [ -z ${CONFIG_FILE+x} ]; 
+then echo "Configuration-File is not set"; 
+else commandCollector="$commandCollector --config=$CONFIG_FILE "; 
+fi
+
+if [ -z ${CONFIG_OVERRIDE+x} ]; 
+then echo "No Configuration Override given"; 
+else commandCollector="$commandCollector --override=$CONFIG_OVERRIDE "; 
+fi
+
+if [ -z ${NO_TIMESTAMP+x} ]; 
+then echo "TimeStamps will be printed"; 
+else commandCollector="$commandCollector --no-log-timestamp "; 
+fi
+
+if [ -z ${SEED+x} ]; 
+then echo ""; 
+else commandCollector="$commandCollector --seed=$SEED "; 
 fi
 
 echo "final cmd is ${commandCollector}"
@@ -17,5 +42,5 @@ echo "final cmd is ${commandCollector}"
 /bin/bash -c "$commandCollector"
 
 # To keep the container open for inspection
-# echo "Keeping Container open for inspection"
+# echo "Program finished - Keeping Container open for inspection"
 # tail -f /dev/null
