138fe2dc58d4dee6a719d75d773d0deac71ad245
Matthías Páll Gissurarson <mpg@mpg.is>
2021-07-22 16:31:59 +0000

    [Minor] Clean up slow-tests

--- tests/SlowTests.hs
+++ tests/SlowTests.hs
@@ -7,10 +7,11 @@ module Main where
 import Data.Default
 import Data.IORef (readIORef)
 import Data.Maybe (isJust, mapMaybe)
+import Data.Set (Set)
 import qualified Data.Set as Set
 import Endemic (getExprFitCands)
 import Endemic.Configuration
-import Endemic.Diff (applyFixes, getFixBinds, ppDiff)
+import Endemic.Diff (applyFixes, fixesToDiffs, getFixBinds, ppDiff)
 import Endemic.Eval
 import Endemic.Search
 import Endemic.Search.PseudoGenetic (pseudoGeneticRepair)
@@ -35,187 +36,125 @@ tESTSEED = 703_039_772
 tESTGENCONF :: GeneticConfiguration
 tESTGENCONF = def {crossoverRate = 0.95, mutationRate = 0.05, dropRate = 0.05}
 
+mkRepairTest :: (ProblemDescription -> IO (Set EFix)) -> Integer -> TestName -> FilePath -> [String] -> TestTree
+mkRepairTest how timeout tag file expected =
+  localOption (mkTimeout timeout) $
+    testCase tag $ do
+      setSeedGenSeed (tESTSEED + 5)
+      desc <- describeProblem def file
+      fixes <- how desc
+      fixesToDiffs desc fixes @?= expected
+
+mkGenConfTest :: Integer -> TestName -> FilePath -> [String] -> TestTree
+mkGenConfTest = mkRepairTest (\desc -> runGenMonad tESTGENCONF desc tESTSEED geneticSearchPlusPostprocessing)
+
+mkSearchTest :: SearchAlgorithm -> Integer -> TestName -> FilePath -> [String] -> TestTree
+mkSearchTest search_conf = mkRepairTest (runRepair search_conf)
+
+mkPseudoGenTest :: Integer -> TestName -> FilePath -> [String] -> TestTree
+mkPseudoGenTest = mkRepairTest (pseudoGeneticRepair def)
+
 tastyFixTests :: TestTree
 tastyFixTests =
   testGroup
     "Tasty fix tests"
-    [ localOption (mkTimeout 30_000_000) $
-        testCase "Repair TastyFix" $ do
-          let toFix = "tests/cases/TastyFix.hs"
-              repair_target = Nothing
-              expected =
-                map
-                  unlines
-                  [ [ "diff --git a/tests/cases/TastyFix.hs b/tests/cases/TastyFix.hs",
-                      "--- a/tests/cases/TastyFix.hs",
-                      "+++ b/tests/cases/TastyFix.hs",
-                      "@@ -7,1 +7,1 @@ x = 2",
-                      "-x = 2",
-                      "+x = 3"
-                    ]
-                  ]
-
-          setSeedGenSeed (tESTSEED + 5)
-          (_, modul, [EProb {..}]) <-
-            moduleToProb (def {packages = def packages ++ ["tasty", "tasty-hunit"]}) toFix repair_target
-          desc <- describeProblem def toFix
-          fixes <- runGenMonad tESTGENCONF desc tESTSEED geneticSearchPlusPostprocessing
-          let fixProgs = map (`replaceExpr` progAtTy e_prog e_ty) $ Set.toList fixes
-              fixDiffs = map (concatMap ppDiff . snd . applyFixes modul . getFixBinds) fixProgs
-          fixDiffs @?= expected,
-      localOption (mkTimeout 30_000_000) $
-        testCase "Repair TastyMix" $ do
-          let toFix = "tests/cases/TastyMix.hs"
-              repair_target = Nothing
-              expected =
-                map
-                  unlines
-                  [ [ "diff --git a/tests/cases/TastyMix.hs b/tests/cases/TastyMix.hs",
-                      "--- a/tests/cases/TastyMix.hs",
-                      "+++ b/tests/cases/TastyMix.hs",
-                      "@@ -7,1 +7,1 @@ x = 2",
-                      "-x = 2",
-                      "+x = 3"
-                    ]
-                  ]
-
-          setSeedGenSeed tESTSEED
-          (_, modul, [EProb {..}]) <-
-            moduleToProb (def {packages = def packages ++ ["tasty", "tasty-hunit"]}) toFix repair_target
-          desc <- describeProblem def toFix
-          fixes <- runGenMonad tESTGENCONF desc tESTSEED geneticSearchPlusPostprocessing
-          let fixProgs = map (`replaceExpr` progAtTy e_prog e_ty) $ Set.toList fixes
-              fixDiffs = map (concatMap ppDiff . snd . applyFixes modul . getFixBinds) fixProgs
-          fixDiffs @?= expected
+    [ mkGenConfTest 30_000_000 "Repair TastyFix" "tests/cases/TastyFix.hs" $
+        map
+          unlines
+          [ [ "diff --git a/tests/cases/TastyFix.hs b/tests/cases/TastyFix.hs",
+              "--- a/tests/cases/TastyFix.hs",
+              "+++ b/tests/cases/TastyFix.hs",
+              "@@ -7,1 +7,1 @@ x = 2",
+              "-x = 2",
+              "+x = 3"
+            ]
+          ],
+      mkGenConfTest 30_000_000 "Repair TastyMix" "tests/cases/TastyMix.hs" $
+        map
+          unlines
+          [ [ "diff --git a/tests/cases/TastyMix.hs b/tests/cases/TastyMix.hs",
+              "--- a/tests/cases/TastyMix.hs",
+              "+++ b/tests/cases/TastyMix.hs",
+              "@@ -7,1 +7,1 @@ x = 2",
+              "-x = 2",
+              "+x = 3"
+            ]
+          ]
     ]
 
 randTests :: TestTree
 randTests =
   testGroup
     "Random search tests"
-    [ localOption (mkTimeout 60_000_000) $
-        testCase "Repair TastyFix" $ do
-          let toFix = "tests/cases/TastyFix.hs"
-              expected =
-                map
-                  unlines
-                  [ [ "diff --git a/tests/cases/TastyFix.hs b/tests/cases/TastyFix.hs",
-                      "--- a/tests/cases/TastyFix.hs",
-                      "+++ b/tests/cases/TastyFix.hs",
-                      "@@ -7,1 +7,1 @@ x = 2",
-                      "-x = 2",
-                      "+x = 3"
-                    ]
-                  ]
-              randConf = def {randStopOnResults = True, randIgnoreFailing = True}
-          setSeedGenSeed (tESTSEED + 5)
-          fixes <- describeProblem def toFix >>= runRepair (Random randConf)
-          (_, modul, [EProb {..}]) <- moduleToProb def toFix Nothing
-          let fixProgs = map (`replaceExpr` progAtTy e_prog e_ty) $ Set.toList fixes
-              fixDiffs = map (concatMap ppDiff . snd . applyFixes modul . getFixBinds) fixProgs
-          fixDiffs @?= expected
+    [ let conf = Random def {randStopOnResults = True, randIgnoreFailing = True}
+       in mkSearchTest conf 60_000_000 "Repair TastyFix" "tests/cases/TastyFix.hs" $
+            map
+              unlines
+              [ [ "diff --git a/tests/cases/TastyFix.hs b/tests/cases/TastyFix.hs",
+                  "--- a/tests/cases/TastyFix.hs",
+                  "+++ b/tests/cases/TastyFix.hs",
+                  "@@ -7,1 +7,1 @@ x = 2",
+                  "-x = 2",
+                  "+x = 3"
+                ]
+              ]
     ]
 
 properGenTests :: TestTree
 properGenTests =
   testGroup
     "proper generation tests"
-    [ localOption (mkTimeout 60_000_000) $
-        testCase "Repair ThreeFixes" $ do
-          let toFix = "tests/cases/ThreeFixes.hs"
-              repair_target = Nothing
-              expected =
-                map
-                  unlines
-                  [ [ "diff --git a/tests/cases/ThreeFixes.hs b/tests/cases/ThreeFixes.hs",
-                      "--- a/tests/cases/ThreeFixes.hs",
-                      "+++ b/tests/cases/ThreeFixes.hs",
-                      "@@ -20,1 +20,1 @@ brokenPair = (1, 2, 3)",
-                      "-brokenPair = (1, 2, 3)",
-                      "+brokenPair = (3, 4, 5)"
-                    ]
-                  ]
-          setSeedGenSeed (tESTSEED + 5)
-          (_, modul, [EProb {..}]) <- moduleToProb def toFix repair_target
-          desc <- describeProblem def toFix
-          fixes <- runGenMonad tESTGENCONF desc tESTSEED geneticSearchPlusPostprocessing
-          let fixProgs = map (`replaceExpr` progAtTy e_prog e_ty) $ Set.toList fixes
-              fixDiffs = map (concatMap ppDiff . snd . applyFixes modul . getFixBinds) fixProgs
-          fixDiffs @?= expected
+    [ mkGenConfTest 60_000_000 "Repair ThreeFixes" "tests/cases/ThreeFixes.hs" $
+        map
+          unlines
+          [ [ "diff --git a/tests/cases/ThreeFixes.hs b/tests/cases/ThreeFixes.hs",
+              "--- a/tests/cases/ThreeFixes.hs",
+              "+++ b/tests/cases/ThreeFixes.hs",
+              "@@ -20,1 +20,1 @@ brokenPair = (1, 2, 3)",
+              "-brokenPair = (1, 2, 3)",
+              "+brokenPair = (3, 4, 5)"
+            ]
+          ]
     ]
 
 genTests :: TestTree
 genTests =
   testGroup
     "Generation tests"
-    [ localOption (mkTimeout 120_000_000) $
-        testCase "Repair TwoFixes" $ do
-          let toFix = "tests/cases/TwoFixes.hs"
-              repair_target = Nothing
-              expected =
-                map
-                  unlines
-                  [ [ "diff --git a/tests/cases/TwoFixes.hs b/tests/cases/TwoFixes.hs",
-                      "--- a/tests/cases/TwoFixes.hs",
-                      "+++ b/tests/cases/TwoFixes.hs",
-                      "@@ -12,1 +12,1 @@ brokenPair = (1, 2)",
-                      "-brokenPair = (1, 2)",
-                      "+brokenPair = (3, 4)"
-                    ]
-                  ]
-
-          setSeedGenSeed (tESTSEED + 5)
-          (cc', mod, [tp@EProb {..}]) <- moduleToProb def toFix repair_target
-          desc <- describeProblem def toFix
-          fixes <- pseudoGeneticRepair def desc
-          let fixProgs = map (`replaceExpr` progAtTy e_prog e_ty) $ Set.toList fixes
-              fixDiffs = map (concatMap ppDiff . snd . applyFixes mod . getFixBinds) fixProgs
-          fixDiffs @?= expected,
-      localOption (mkTimeout 75_000_000) $
-        testCase "Repair ThreeFixes" $ do
-          let toFix = "tests/cases/ThreeFixes.hs"
-              repair_target = Nothing
-              expected =
-                map
-                  unlines
-                  [ [ "diff --git a/tests/cases/ThreeFixes.hs b/tests/cases/ThreeFixes.hs",
-                      "--- a/tests/cases/ThreeFixes.hs",
-                      "+++ b/tests/cases/ThreeFixes.hs",
-                      "@@ -20,1 +20,1 @@ brokenPair = (1, 2, 3)",
-                      "-brokenPair = (1, 2, 3)",
-                      "+brokenPair = (3, 4, 5)"
-                    ]
-                  ]
-
-          setSeedGenSeed (tESTSEED + 5)
-          (cc', mod, [tp@EProb {..}]) <- moduleToProb def toFix repair_target
-          desc <- describeProblem def toFix
-          fixes <- pseudoGeneticRepair def desc
-          let fixProgs = map (`replaceExpr` progAtTy e_prog e_ty) $ Set.toList fixes
-              fixDiffs = map (concatMap ppDiff . snd . applyFixes mod . getFixBinds) fixProgs
-          fixDiffs @?= expected,
-      localOption (mkTimeout 90_000_000) $
-        testCase "Repair FourFixes" $ do
-          let toFix = "tests/cases/FourFixes.hs"
-              repair_target = Nothing
-              expected =
-                map
-                  unlines
-                  [ [ "diff --git a/tests/cases/FourFixes.hs b/tests/cases/FourFixes.hs",
-                      "--- a/tests/cases/FourFixes.hs",
-                      "+++ b/tests/cases/FourFixes.hs",
-                      "@@ -24,1 +24,1 @@ brokenPair = (1, 2, 3, 4)",
-                      "-brokenPair = (1, 2, 3, 4)",
-                      "+brokenPair = (3, 4, 5, 6)"
-                    ]
-                  ]
-          setSeedGenSeed (tESTSEED + 5)
-          (cc', mod, [tp@EProb {..}]) <- moduleToProb def toFix repair_target
-          desc <- describeProblem def toFix
-          fixes <- pseudoGeneticRepair def desc
-          let fixProgs = map (`replaceExpr` progAtTy e_prog e_ty) $ Set.toList fixes
-              fixDiffs = map (concatMap ppDiff . snd . applyFixes mod . getFixBinds) fixProgs
-          fixDiffs @?= expected
+    [ mkPseudoGenTest 120_000_000 "Repair TwoFixes" "tests/cases/TwoFixes.hs" $
+        map
+          unlines
+          [ [ "diff --git a/tests/cases/TwoFixes.hs b/tests/cases/TwoFixes.hs",
+              "--- a/tests/cases/TwoFixes.hs",
+              "+++ b/tests/cases/TwoFixes.hs",
+              "@@ -12,1 +12,1 @@ brokenPair = (1, 2)",
+              "-brokenPair = (1, 2)",
+              "+brokenPair = (3, 4)"
+            ]
+          ],
+      mkPseudoGenTest 75_000_000 "Repair ThreeFixes" "tests/cases/ThreeFixes.hs" $
+        map
+          unlines
+          [ [ "diff --git a/tests/cases/ThreeFixes.hs b/tests/cases/ThreeFixes.hs",
+              "--- a/tests/cases/ThreeFixes.hs",
+              "+++ b/tests/cases/ThreeFixes.hs",
+              "@@ -20,1 +20,1 @@ brokenPair = (1, 2, 3)",
+              "-brokenPair = (1, 2, 3)",
+              "+brokenPair = (3, 4, 5)"
+            ]
+          ],
+      mkPseudoGenTest 90_000_000 "Repair FourFixes" "tests/cases/FourFixes.hs" $
+        map
+          unlines
+          [ [ "diff --git a/tests/cases/FourFixes.hs b/tests/cases/FourFixes.hs",
+              "--- a/tests/cases/FourFixes.hs",
+              "+++ b/tests/cases/FourFixes.hs",
+              "@@ -24,1 +24,1 @@ brokenPair = (1, 2, 3, 4)",
+              "-brokenPair = (1, 2, 3, 4)",
+              "+brokenPair = (3, 4, 5, 6)"
+            ]
+          ]
     ]
 
+main :: IO ()
 main = defaultMain tests
