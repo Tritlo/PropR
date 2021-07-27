37a730cf5e94a60f0d3918598c17eea4a356935b
Matthías Páll Gissurarson <mpg@mpg.is>
2021-07-27 16:41:20 +0000

    [Minor] Fix non-external package tests

--- tests/cases/packages/TestPackage/tests/Tests.hs
+++ tests/cases/packages/TestPackage/tests/Tests.hs
@@ -1,11 +1,13 @@
 module Main where
 
-import Test.Package
 import Test.Tasty
 import Test.Tasty.HUnit
 
+y :: Int
+y = 12
+
 tests :: TestTree
-tests = testCase "Is the answer" (x @?= 42)
+tests = testCase "Is the answer" (y @?= 42)
 
 main :: IO ()
 main = defaultMain tests
